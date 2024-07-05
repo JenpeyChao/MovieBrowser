96a46bdde77560d3f6bd10f84475d72b279994db
aaron <redskin.rox@gmail.com>
2024-07-05 10:02:44 -0400

    movie comp init

--- src/app/movie-browser/movie-browser.component.ts
+++ src/app/movie-browser/movie-browser.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { MovieBrowserService } from '../movie-browser.service';
 @Component({
   selector: 'app-movie-browser',
   templateUrl: './movie-browser.component.html',
   styleUrl: './movie-browser.component.css'
 })
 export class MovieBrowserComponent {
+  movieData:any = [];
+  year = '';
+  name = '';
+  constructor(private MovieBrowserService:MovieBrowserService){}
 
+  searchMovie(){}
 }
