ed309ffe29da2919a55c7cd6aa47d8bd23d0ecb7
aaron <redskin.rox@gmail.com>
2024-07-05 15:01:55 -0400

    new comp

---
+++ src/app/one-movie/one-movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-movie',
+  templateUrl: './one-movie.component.html',
+  styleUrl: './one-movie.component.css'
+})
+export class OneMovieComponent {
+
+}
