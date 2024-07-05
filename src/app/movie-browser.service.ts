a449006fa3d420781704d7bfba2ebda7272950d0
pokeman450 <tman45048@gmail.com>
2024-07-05 10:23:45 -0400

    Updated movie service

--- src/app/movie-browser.service.ts
+++ src/app/movie-browser.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieBrowserService {
 
-  constructor() { }
+  private ApiKey ='7a0248e4';
+  constructor(private http: HttpClient) { }
+
+  searchForMovie(search:string, year:number){
+    if(year!=undefined){
+      return this.http.get(`https://omdbapi.com/?apikey=${this.ApiKey}&t=${search}&y=${year}`);
+    } else{
+    return this.http.get(`https://omdbapi.com/?apikey=${this.ApiKey}&t=${search}`);
+    }
+  }
 }
