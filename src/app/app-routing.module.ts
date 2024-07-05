6517b8b92ce9a5d1e0a4801ca962cecd32367f64
aaron <redskin.rox@gmail.com>
2024-07-05 09:42:23 -0400

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieBrowserComponent } from './movie-browser/movie-browser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:MovieBrowserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
